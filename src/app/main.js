412ace45e08730635f40d20a03b70633f4bbdf0c
Josh D Miller <josh@joshdmiller.com>
2012-03-07 14:55:12 -0800

    Basic style framework

--- src/app/main.js
+++ src/app/main.js
@@ -21,27 +21,7 @@ define([ 'dojo/has', 'require' ], function (has, require) {
     var app = {};
 
     if (has('host-browser')) {
-        /*
-         * This require call’s first dependency, “./Dialog”, uses a relative module identifier; you should use this
-         * type of notation for dependencies *within* a package in order to ensure the package is fully portable. It
-         * works like a path, where “./” refers to the current directory and “../” refers to the parent directory. If
-         * you are referring to a module in a *different* package, you should *not* use a relative module identifier.
-         *
-         * The second dependency is a plugin dependency; in this case, it is a dependency on the special functionality
-         * of the “dojo/domReady” plugin, which waits until the DOM is ready before finishing loading.
-         * The “!” after the module name indicates you want to use special plugin functionality; if you were to
-         * require just “dojo/domReady”, it would load that module just like any other module, without any of the
-         * special plugin functionality.
-         */
-        require([ 'citeplasm/Dialog', 'dojo/domReady!' ], function (Dialog) {
-            app.dialog = new Dialog().placeAt(document.body);
-
-            // It is important to remember to always call startup on widgets after you have added them to the DOM.
-            // It won’t hurt if you do it twice, but things will often not work right if you forget to do it.
-            app.dialog.startup();
-
-            // And now…
-            app.dialog.show();
+        require([ 'dojo/domReady!' ], function () {
         });
     }
     else {
